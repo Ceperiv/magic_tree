3ca4096e3ce6fb7639145a998102b91c1837b55a
Andriy Konoval <ceperiv@gmail.com>
2023-10-06 18:27:11 +0300

    404 added

--- src/components/error404/error404.tsx
+++ src/components/error404/error404.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './error404.scss';
+import error404Image from '../../assets/error_404.jpg';
 
 function Error404() {
     return (
-        <div>
-            Error404
+        <div className={'err_404'}>
+            <Link className={'nav_link'} to="/main"><i>Go to Main</i></Link>
+            <img src={error404Image} alt="Error 404" />
         </div>
-
     );
 }
 
-export {Error404};
+export { Error404 };
